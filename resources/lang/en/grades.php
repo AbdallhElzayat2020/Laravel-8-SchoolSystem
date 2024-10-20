c82cfeb75488bd8479b9eb09f13146f8aa8e022f
Abdallh ELzayat <121009680+AbdallhElzayat2020@users.noreply.github.com>
2024-10-20 18:11:55 +0300

    Laravel 8 SchoolSystem

---
+++ resources/lang/en/grades.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'title_page' => 'Grades',
+    'List_Grade' => 'List_Grade',
+    'Notes_en' => 'Notes_en',
+    'Notes_ar' => 'Notes_ar',
+
+    'Name' => 'Name',
+    'Processes' => 'Processes',
+
+    'add_Grade' => 'Add Grade',
+    'edit_Grade' => 'edit_Grade',
+    'delete_Grade' => 'delete_Grade',
+    'Warning_Grade' => 'Are Sure Of The Deleting Process ?',
+    'stage_name_ar' => 'stage_name_ar',
+    'stage_name_en' => 'stage_name_en',
+    'submit' => 'submit',
+    'delete_Grade_Error' => 'The Grade cannot be deleted because there are classes attached to it',
+    'Edit' => 'Edit',
+    'successly' => 'successly Added',
+    'Already_Exist' => 'This Grade Already Exist',
+
+    'exists' => 'This field already exists',
+    'Delete' => 'Delete',
+    'Close' => 'Close',
+];
+
